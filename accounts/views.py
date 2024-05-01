7542436ce1193f71c6933d767a374698e948f7fa
Juhyeon Jeong <wwg6710@naver.com>
2024-05-02 01:15:57 +0900

    프로필 조회 기능 완료 !

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from accounts.models import User
 from accounts.serializers import AccountSerializer
 from rest_framework.response import Response
 from rest_framework import status
@@ -14,5 +15,13 @@ class AccountSignupAPIView(APIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
 
-    
+class AccountProfileAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, username):
+        user = User.objects.get(username=username)
+        serializer = AccountSerializer(user)
+        return Response(serializer.data)
+
